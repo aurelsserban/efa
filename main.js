9e1bfafc27ad026077c75d7873d2eb9e10a5ed4c
Aurel Serban <aurels@int.4psa.com>
2024-03-15 15:21:12 +0200

    IPC communication between renderer and main processes

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('node:path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
 
+    // win.loadURL('https://hg.hubgets.com/');
     win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
     createWindow();
+    ipcMain.handle('ping', () => 'pong');
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
